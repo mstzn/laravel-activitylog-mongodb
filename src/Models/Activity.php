f7cae1a65cc75f9894d9cff391e9d216cf3c7437
Mesut UZUN <m.uzun@dceed.com>
2022-03-04 12:36:59 +0300

    update Activity model to use mongodb model

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -3,7 +3,7 @@
 namespace Spatie\Activitylog\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
